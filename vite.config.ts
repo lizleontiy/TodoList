d246c0bd9baa1dd357b71ad9bb9e88cba12377c5
liz <leontiy17@gmail.com>
2023-10-13 18:01:23 +0300

    Added eslint

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker';
+
+export default defineConfig({
+  plugins: [checker({
+    eslint: {
+      lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+    }
+  })]
+})
